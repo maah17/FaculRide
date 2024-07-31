7a0dccdfc1944f5d19d29e1baf505573c161002a
VictorTes <victortesseroli2@gmail.com>
2024-07-31 20:44:13 -0300

    teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('max steel x polly pocket')
+  res.send('bob o construtor')
 })
 
 app.listen(3000)
