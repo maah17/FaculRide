95db185652431c59ebec0effa16b9163b605eb2e
VictorTes <victortesseroli2@gmail.com>
2024-08-19 22:05:51 -0300

    consertado o link do db e tables

--- consulta.js
+++ consulta.js
@@ -2,9 +2,9 @@ const db = require('./cnx')
 
 async function listar() {
     await db.connect()
-    resultado = await db.query('SELECT * FROM curso;')
+    resultado = await db.query('SELECT * FROM clientes;')
     console.log(resultado.rows)
-    resultado2 = await db.query('SELECT * FROM aluno;')
+    resultado2 = await db.query('SELECT * FROM companhia;')
     console.log(resultado2.rows)
     await db.end()
 }
