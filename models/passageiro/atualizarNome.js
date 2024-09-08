4a574193a96c7557c815e7376e46c8afca73925c
VictorTes <victortesseroli2@gmail.com>
2024-09-08 13:38:08 -0300

    conserto atualização de nome e telefone ao mesmo tempo

--- models/passageiro/atualizarNome.js
+++ models/passageiro/atualizarNome.js
@@ -7,10 +7,8 @@ const atualizarNome = async function (nome, nome_antigo, res) {
         const values = [nome, nome_antigo];
         await client.query(query, values);
         console.log(`Passageiro com nome ${nome} att com sucesso.`);
-        res.status(200).send(`Passageiro com nome ${nome} atualizado com sucesso.`);
     } catch (err) {
         console.error('Erro ao att nome:', err);
-        res.status(500).send('Erro ao atualizar o nome.');
     } finally {
         client.release();
     }
--- models/passageiro/atualizarTelefone.js
+++ models/passageiro/atualizarTelefone.js
@@ -7,11 +7,9 @@ const atualizarTelefone = async function (telefone, telefone_antigo, res) {
         const values = [telefone, telefone_antigo];
         await client.query(query, values);
         console.log(`Passageiro com telefone ${telefone} att com sucesso.`);
-        res.status(200).send(`Passageiro com telefone ${telefone} atualizado com sucesso.`);
     }
     catch (err) {
         console.error('Erro ao att telefone:', err);
-        res.status(500).send('Erro ao atualizar o telefone.');
     } finally {
         client.release();
     }
